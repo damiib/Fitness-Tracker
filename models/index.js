6ac2facdf18ef32a7403b4ad3a4ee1aa8b50fd14
Damali <brown@MacBook-Pro.local>
2021-11-18 19:14:36 -0800

    added models from class exercises

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// Exporting an object containing all of our models
+// TJ's class notes
+module.exports = {
+    Note: require("./Note"),
+    User: require("./User")
+  };
---
+++ models/workout.js
@@ -0,0 +1,21 @@
+// TJ's class notes
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    unique: true
+  },
+  notes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Note"
+    }
+  ]
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
